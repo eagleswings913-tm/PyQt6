6d88d7d83c32eb579a9cf80e6d549f29caec4c27
Tricia Miller <eagleswings913@gmail.com>
2026-06-23 18:34:22 -0400

    PyQt6 layouts - QHBoxLayout, QVBoxLayout, and nested layouts

---
+++ Layout/Nesting_Layouts.py
@@ -0,0 +1,44 @@
+
+from layout_colorwidget import Color
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QWidget, QVBoxLayout
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        layout1 = QHBoxLayout()
+        layout2 = QVBoxLayout()
+        layout3 = QVBoxLayout()
+
+        layout2.addWidget(Color("red"))
+        layout2.addWidget(Color("yellow"))
+        layout2.addWidget(Color("purple"))
+
+        layout1.addLayout(layout2)
+
+        layout1.addWidget(Color("green"))
+
+        layout3.addWidget(Color("red"))
+        layout3.addWidget(Color("purple"))
+
+        layout1.addLayout(layout3)
+
+        # set spacing around the layout, outside margins
+        layout1.setContentsMargins(0,0,0,0)
+        # set spacing between elements
+        # layout1.setSpacing(0)
+        layout1.setSpacing(20)
+
+        widget = QWidget()
+        widget.setLayout(layout1)
+        self.setCentralWidget(widget)
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
---
+++ Layout/QHBoxLayout.py
@@ -0,0 +1,27 @@
+
+from layout_colorwidget import Color
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        layout = QHBoxLayout()
+
+        layout.addWidget(Color("red"))
+        layout.addWidget(Color("green"))
+        layout.addWidget(Color("blue"))
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
